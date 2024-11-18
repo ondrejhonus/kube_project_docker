9b27dfa56965caaa782dbc530de3c455fae12628
Ondrej Honus - Laptop <ondrejhonus@proton.me>
2024-11-18 08:48:06 +0100

    testing branch

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! - new version'
+    return 'This is a testing branch for developing'
 
 if __name__ == '__main__':
     app.run(debug=True)
